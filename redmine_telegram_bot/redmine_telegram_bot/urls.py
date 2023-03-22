b73c39af27ac2463ab2110a7fe2791a146ab17fd
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-03-22 22:47:34 +0300

    Build

--- redmine_telegram_bot/redmine_telegram_bot/urls.py
+++ redmine_telegram_bot/redmine_telegram_bot/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from redmine import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bot_webhook/',
+         csrf_exempt(views.TelegramBotWebhookView.as_view()))
 ]
