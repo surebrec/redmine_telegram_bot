f179dba953a3040796e5b85e1c589f4fc63d4523
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-03-22 23:06:39 +0300

    Build

--- redmine_telegram_bot/telegram_bot/handlers/start_handler.py
+++ redmine_telegram_bot/telegram_bot/handlers/start_handler.py
@@ -11,7 +11,6 @@ django.setup()
 def command_start(update: Update, context: CallbackContext) -> None:
     template_text = 'Привет, {first_name}.'
     text = template_text.format(
-        first_name=update.effective_user.username,
-        parse_mode=ParseMode.MARKDOWN_V2
+        first_name=update.effective_user.username
     )
-    update.message.reply_text(text=text, parse_mode=ParseMode.MARKDOWN_V2)
+    update.message.reply_text(text=text)
