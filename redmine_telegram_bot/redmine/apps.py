f167bf2e275f8a52cc867e1af35860ff2256040b
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-05-09 11:49:59 +0300

    Apps configs changed

--- redmine_telegram_bot/redmine/apps.py
+++ redmine_telegram_bot/redmine/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RedmineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'redmine'
+
+    def ready(self):
+        import redmine.signals
