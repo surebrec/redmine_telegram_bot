b65a4e792635f0168f6103551bf4ec53c27efcdc
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-05-09 11:48:56 +0300

    "start" & "stop" command created

--- redmine_telegram_bot/telegram_bot/handlers/start_handler.py
+++ redmine_telegram_bot/telegram_bot/handlers/start_handler.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 import django
 from telegram import Update
@@ -13,20 +12,15 @@ django.setup()
 
 
 def command_start(update: Update, context: CallbackContext) -> None:
-    template_text = 'Привет, {first_name}.'
-    text = template_text.format(
-        first_name=update.effective_user.username,
-        chat_id=update.effective_chat.id,
-    )
     chat, created = Chat.objects.get_or_create(
-        name='default',
-        chat_id=update.effective_chat.id,
-        is_active=True)
-    if not created:
-        if not chat.is_active:
-            chat.is_active = True
-            chat.save()
-        text = 'Бот уже работает)'
+        chat_id=update.effective_chat.id)
+    if created or not chat.is_active:
+        chat.is_active = True
+        chat.save()
+        text = 'Бот начинает свою работу'
+    else:
+        text = 'Бот уже работает'
+
     update.message.reply_text(text=text)
 
 
@@ -38,5 +32,4 @@ def command_stop(update: Update, context: CallbackContext) -> None:
         text = 'Бот остановлен...'
     except Chat.DoesNotExist:
         text = 'Бот уже остановлен('
-    finally:
-        update.message.reply_text(text=text)
+    update.message.reply_text(text=text)
