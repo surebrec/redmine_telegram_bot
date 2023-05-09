e3b288c253b62ad2c1cde8c57f363269d957f136
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-05-09 11:50:12 +0300

    Apps configs changed

--- redmine_telegram_bot/redmine_telegram_bot/celery.py
+++ redmine_telegram_bot/redmine_telegram_bot/celery.py
@@ -1,11 +1,12 @@
 import os
+from django.conf import settings
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'redmine_telegram_bot.settings')
 
 app = Celery('redmine_telegram_bot')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object(settings, namespace='CELERY')
 app.autodiscover_tasks()
 
 
