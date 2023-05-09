9a295b44b677d3f541ab4f6acc708a5fd98bb987
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-05-09 11:44:40 +0300

    Webhook viewset added

--- redmine_telegram_bot/redmine/views.py
+++ redmine_telegram_bot/redmine/views.py
@@ -1,17 +1,9 @@
-import json
-from .tasks import process_telegram_event
-from django.http import JsonResponse
-from django.views import View
-from django.conf import settings
+from rest_framework import viewsets
+
+from redmine.models import RedmineGroup
+from redmine.serializers import RedmineGroupSerializer
 
 
-class TelegramBotWebhookView(View):
-    def post(self, request, *args, **kwargs):
-        if settings.DEBUG:
-            process_telegram_event(json.loads(request.body))
-        else:
-            process_telegram_event.delay(json.loads(request.body))
-        return JsonResponse({"ok": "POST received"})
-
-    def get(self, request, *args, **kwargs):
-        return JsonResponse({"ok": "chilling"})
+class RedmineGroupViewSet(viewsets.ModelViewSet):
+    queryset = RedmineGroup.objects.all()
+    serializer_class = RedmineGroupSerializer
--- redmine_telegram_bot/telegram_bot/views.py
+++ redmine_telegram_bot/telegram_bot/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import json
+from .tasks import process_telegram_event
+from django.http import JsonResponse
+from django.views import View
+from django.conf import settings
 
-# Create your views here.
+
+class TelegramBotWebhookView(View):
+    def post(self, request, *args, **kwargs):
+        if settings.DEBUG:
+            process_telegram_event(json.loads(request.body))
+        else:
+            process_telegram_event.delay(json.loads(request.body))
+        return JsonResponse({"ok": "POST received"})
+
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({"ok": "chilling"})
