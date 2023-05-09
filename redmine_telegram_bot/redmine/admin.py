339f8f8f954123fef79f867d4bf16a70a9167e1b
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-05-09 11:38:56 +0300

    Models registration in admin panel

--- redmine_telegram_bot/redmine/admin.py
+++ redmine_telegram_bot/redmine/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import RedmineGroup, RedmineUser
 
-# Register your models here.
+
+class RedmineGroupAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_valid']
+    readonly_fields = ['name', 'is_valid']
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None and obj.is_valid:
+            return False
+        return super().has_change_permission(request, obj=obj)
+
+
+admin.site.register(RedmineGroup, RedmineGroupAdmin)
+
+
+class RedmineUserAdmin(admin.ModelAdmin):
+    list_display = ['short_name', 'group']
+
+
+admin.site.register(RedmineUser, RedmineUserAdmin)
--- redmine_telegram_bot/telegram_bot/admin.py
+++ redmine_telegram_bot/telegram_bot/admin.py
@@ -2,8 +2,28 @@ from django.contrib import admin
 from .models import Chat
 
 
+class RedmineGroupInline(admin.TabularInline):
+    model = Chat.groups.through
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None:
+            return False
+        return super().has_change_permission(request, obj=obj)
+
+
+class TaskInline(admin.TabularInline):
+    model = Chat.tasks.through
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None:
+            return False
+        return super().has_change_permission(request, obj=obj)
+
+
 class ChatAdmin(admin.ModelAdmin):
+    inlines = [RedmineGroupInline, TaskInline]
     list_display = ('name', 'is_active')
+    readonly_fields = ['chat_id', ]
 
 
 admin.site.register(Chat, ChatAdmin)
