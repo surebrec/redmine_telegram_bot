b03fa2e5ff7f8676a8ca8faadb7d31bfa0e71806
Михаил Касаткин <m.m.kasatkin@gmail.com>
2023-06-20 14:01:43 +0300

    requirements updated

--- redmine_telegram_bot/redmine_telegram_bot/settings.py
+++ redmine_telegram_bot/redmine_telegram_bot/settings.py
@@ -185,12 +185,8 @@ if DEBUG:
         "127.0.0.1", "10.0.2.2"]
 
 
-def show_toolbar(request):
-    return True
-
-
 DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+    'SHOW_TOOLBAR_CALLBACK': lambda request: DEBUG,
 }
 
 CACHE_TIME = 60
